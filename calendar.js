da420c5459296229ecf9590fa2de1249345f7d2f
adamberman <adamberman13@gmail.com>
2015-02-09 16:44:14 -0800

    step 1 of debugging multi-events in same time

--- calendar.js
+++ calendar.js
@@ -2,20 +2,38 @@
 // in minutes after 9:00 AM. Also is built to handle Event Title
 // and Event Location.
 
-// Required Args: startTime (number), endTime (number)
+// Required Args: start (number), end (number)
 // Optional Args: eventTitle (string), eventLocation (string)
 
-// Example: { startTime: 90, endTime: 180 }
-// Or: { startTime: 120, endTime: 240, eventTitle: "Brunch", eventLocation: "The Diner" }
+// Example: [{ start: 90, end: 180 }, {start: 120, end: 150}]
+// Or: [{ start: 120, end: 240, eventTitle: "Brunch", eventLocation: "The Diner" }]
 
 var layOutDay = function (events) {
+
+	document.getElementById('col-2').innerHTML = '';
+
+	function addClass (el, className) {
+		if (el.classList) {
+  			el.classList.add(className);
+			} else {
+  			el.className += ' ' + className;
+		}
+
+		return el;
+	}
+
+	function mergeSort(array) {
+		
+	}
+
+	var sortedEvents = mergeSort(events);
 
 	var eventTimes = [];
 
 	events.forEach(function (event) {
-		if (event.hasOwnProperty('startTime') && event.hasOwnProperty('endTime')) {
-			var start = event.startTime;
-			var end = event.endTime;
+		if (event.hasOwnProperty('start') && event.hasOwnProperty('end')) {
+			var start = event.start;
+			var end = event.end;
 
 			for (start; start <= end; start ++) {
 				if (eventTimes[start]) {
@@ -29,19 +47,20 @@ var layOutDay = function (events) {
 	})
 
 	events.forEach(function (event) {
-		if (event.hasOwnProperty('startTime') && event.hasOwnProperty('endTime')) {
+		if (event.hasOwnProperty('start') && event.hasOwnProperty('end')) {
+
 			var template = document.getElementById('event-template').innerHTML;
 
 			if (event.hasOwnProperty('eventTitle')) {
-				template.replace('Event Title', event.eventTitle);
+				template = template.replace('Event Title', event.eventTitle);
 			}
 
 			if (event.hasOwnProperty('eventLocation')) {
-				template.replace('Event Location', event.eventLocation);
+				template = template.replace('Event Location', event.eventLocation);
 			}
 
-			var start = event.startTime;
-			var end = event.endTime;
+			var start = event.start;
+			var end = event.end;
 			var maxOverlap = 1;
 			var position = 0;
 
@@ -49,26 +68,28 @@ var layOutDay = function (events) {
 				var length = eventTimes[start].pop()
 				if (length > maxOverlap) {
 					maxOverlap = length;
-					if (eventTimes[start].length === 0) {
-						position = 1;
-					}
+				}
+				if (start === end && length > 1) {
+					// && (length > 1 && eventTimes[start].length === 0)
+					position = 1;
 				}
 			}
 
 			var el = document.createElement('DIV');
-			if (el.classList) {
-  			el.classList.add('event');
+			addClass(el, 'event');
+
+			el.style.height = String(event.end - event.start - 2) + 'px';
+
+			if (maxOverlap > 1) {
+				if (position === 0) {
+					addClass(el, 'left');
+				} else {
+					addClass(el, 'right');
+				}
 			} else {
-  			el.className += ' ' + 'event';
+				addClass(el, 'full');
 			}
-			el.style.width = String(594 / maxOverlap) + 'px';
-			el.style.height = String(event.endTime - event.startTime) + 'px';
-			el.style.top = String(15 + event.startTime) + 'px';
-			if (position === 0) {
-				el.style.left = '10px';
-			} else {
-				el.style.left = '310px';
-			}
+			el.style.top = String(event.start) + 'px';
 
 			el.innerHTML = template;
 			document.getElementById('col-2').appendChild(el);
